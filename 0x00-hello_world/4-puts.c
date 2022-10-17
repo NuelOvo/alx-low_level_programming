17e2b8f41eab08f16bc3d45bf25d209a053c4398
Emmanuelovo <ovoworks684@gmail.com>
2022-10-17 06:16:29 -0700

    puts program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#incude < stdio.h >
+
+/**
+ * main - This is the Entry point for the function
+ *
+ * puts: this is the output stream holding our value
+ *
+ * Return: 0 is the expected value, which is true, no error
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
