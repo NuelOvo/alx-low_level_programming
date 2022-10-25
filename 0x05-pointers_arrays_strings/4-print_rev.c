be57705d8698cf853316d531ffb074e5ae07897c
Emmanuelovo <ovoworks684@gmail.com>
2022-10-25 09:16:52 -0700

    Pointers and Array Task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*
+* print_rev - prints string in reverse form
+* @s: string
+* Return: void
+*
+*
+ **/
+
+void print_rev(char *s)
+{
+	int x  = 0;
+
+	while (s[x] != '\0')
+	{
+	x++;
+	}
+	x--;
+	while (x >= 0)
+	{
+	_putchar(s[x]);
+	x--;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ * rev_string - reverse the string
+ * @s: string
+ * Return
+ **/
+
+void rev_string(char *s)
+{
+	int x, max, half;
+	char first; last;
+
+	x = 0;
+	while (s[x] != '\0')
+	{
+	x++;
+	}
+
+	max = x - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+	first = s[max - half];
+	last = s[half];
+	s[half] = first;
+	s[max - half] = last;
+	half--;
+	}
+}
