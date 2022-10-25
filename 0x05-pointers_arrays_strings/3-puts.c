0977ddc7b71cdd171cb4a515143c9b76066ca0b4
Emmanuelovo <ovoworks684@gmail.com>
2022-10-25 09:27:41 -0700

    fix puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 
 /**
  *
- *_puts - prints string
+ * _puts - prints string
  * @str: string
  * Return: void
  *
@@ -11,6 +11,7 @@
 void _puts(char *str)
 {
 	int x = 0;
+
 	while (str[x] != '\0')
 	{
 	_putchar(str[x]);
