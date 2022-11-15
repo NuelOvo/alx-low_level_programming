b12f23c71c5fa785cda9d65093f4496a5422b1b9
Emmanuelovo <ovoworks684@gmail.com>
2022-11-15 07:10:55 -0800

    new

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of argumnents passed into it
+ * main - prints the number of arguments passed into it
  *
  * @argc: argument counter
  * @argv: argument vector
  *
- * Retur: 0
- *
- **/
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +16,7 @@ int main(int argc, char *argv[])
 
 	for (c = 0; c <= argc; c++)
 	{}
-	printf("%d\n",c - 2);
+	printf("%d\n", c - 2);
 
 	return (0);
 }
